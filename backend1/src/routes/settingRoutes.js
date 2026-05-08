df1bedc178e3b9ff6ac26b051b55af731b12eb2b
Rajnit Rai <rai84357@gmail.com>
2026-05-08 11:44:53 +0545

    delete

--- backend1/src/routes/settingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSettings, updateSettings } = require('../controllers/settingController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/', getSettings);
-router.patch('/', protect, updateSettings);
-
-module.exports = router;
